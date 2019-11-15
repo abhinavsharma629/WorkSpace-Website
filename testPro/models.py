69625b1e1a2a8198eba1bc71471fbbea58e91e3d
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-16 04:06:24 +0530

    gd_oauth

--- testPro/models.py
+++ testPro/models.py
@@ -4,3 +4,10 @@ class Tokens(models.Model):
     username=models.CharField(max_length=100)
     access_token=models.CharField(max_length=1000)
     refresh_token=models.CharField(max_length=1000)
+
+
+
+class Temp(models.Model):
+    code=models.CharField(max_length=100)
+    access_token=models.CharField(max_length=1000)
+    username=models.CharField(max_length=100)
